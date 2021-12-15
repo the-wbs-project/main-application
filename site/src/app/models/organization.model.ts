f348cd4a3ba174d3bc99751eed0bdcc33b7f24d8
Chris <chriswalton@cwsoftware.biz>
2021-12-15 14:10:06 -0500

    minor stuff

--- site/src/app/models/organization.model.ts
+++ site/src/app/models/organization.model.ts
@@ -1,8 +1,13 @@
+import { Project } from './project.model';
+import { Wbs } from './wbs.model';
+
 export interface Organization {
   id: string;
   name: string;
+  logo: any;
   seatsPurchased: number;
   administrators: string[];
   seatedUsers: string[];
-
+  projects: Project[];
+  wbsLibrary: Wbs[];
 }
