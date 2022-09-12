294d75a085e7aa8af1867ad029c42d866c4045e2
Chris <chriswalton@cwsoftware.biz>
2022-09-12 08:02:59 +0700

    working on items

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import * as utc from 'dayjs/plugin/utc';
 import * as dayjs from 'dayjs';
+import * as utc from 'dayjs/plugin/utc';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
