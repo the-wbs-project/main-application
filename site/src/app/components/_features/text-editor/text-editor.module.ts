2040cbbdfe322207e7a0efd0308b37c456f83958
Chris <chriswalton@cwsoftware.biz>
2022-11-29 12:53:54 +0700

    fixed the font size issue fur text editor

--- site/src/app/components/_features/text-editor/text-editor.module.ts
+++ site/src/app/components/_features/text-editor/text-editor.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { TextBoxModule } from '@progress/kendo-angular-inputs';
 import { SharedBasicModule } from '@wbs/shared/basic-module';
 import { TextEditorComponent } from './text-editor.component';
+import { TextOverflowHandlerDirective } from './text-overflow-handler.directive';
 
 @NgModule({
   imports: [TextBoxModule, SharedBasicModule],
-  declarations: [TextEditorComponent],
+  declarations: [TextEditorComponent, TextOverflowHandlerDirective],
   exports: [TextEditorComponent],
 })
 export class TextEditorModule {}
