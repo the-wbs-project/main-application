4707aa69051755972eca7d9ac9c532dcb919609e
Chris <chriswalton@cwsoftware.biz>
2023-04-14 14:38:16 +0700

    adding QA

---
+++ site/src/environments/environment.qa.ts
@@ -0,0 +1,30 @@
+import { AuthConfig } from '@auth0/auth0-angular';
+
+export const environment = {
+  production: true,
+  appTitle: 'PM Empower',
+  apiPrefix: 'https://app-test.pm-empower.com/api/',
+  appInsightsKey: '44521f2e-90b6-4be0-8659-0c84bfde1907',
+  auth: <AuthConfig>{
+    domain: 'auth.dev.thewbsproject.com',
+    clientId: '1nOKv3soErPVx2nJlfZFZdIqYkjUnMkX',
+    authorizationParams: {
+      audience: 'https://dev-3lcexa83.us.auth0.com/api/v2/',
+      redirect_uri: `${window.location.protocol}//${window.location.host}`,
+    },
+    // The AuthHttpInterceptor configuration
+    httpInterceptor: {
+      allowedList: [
+        {
+          uri: 'https://app-test.pm-empower.com/api/resources/*',
+          allowAnonymous: true,
+        },
+        {
+          uri: 'https://app-test.pm-empower.com/api/lists/*',
+          allowAnonymous: true,
+        },
+        'https://app-test.pm-empower.com/api/*',
+      ],
+    },
+  },
+};
