9ce62e7e04097fcb70fdf8db57a42dede4318b27
Chris <chriswalton@cwsoftware.biz>
2023-04-18 11:06:19 +0700

    various small fixes

--- site/src/app/layout/components/footer/footer.component.ts
+++ site/src/app/layout/components/footer/footer.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  readonly year = new Date().getFullYear();
+  readonly appTitle = environment.appTitle;
+}
