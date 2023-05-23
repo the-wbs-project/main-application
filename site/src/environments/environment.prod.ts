3ccf1707f3f799df49d1f036608bd1b1d06964c6
Chris <chriswalton@cwsoftware.biz>
2023-05-23 13:01:40 +0700

    fixing env variables for angular

--- site/src/environments/environment.prod.ts
+++ site/src/environments/environment.prod.ts
@@ -1,18 +1,7 @@
 export const environment = {
-  production: false,
+  production: true,
   appTitle: 'PM Empower',
   apiPrefix: 'https://api.pm-empower.com/api/',
   auth_clientId: '1nOKv3soErPVx2nJlfZFZdIqYkjUnMkX',
-  datadog: {
-    clientToken: 'pub3cd763417a98f3cb71ee3ab8d19ddecb',
-    site: 'datadoghq.com',
-    service: 'pm-empower-site',
-    env: 'production',
-    forwardErrorsToLogs: true,
-    sessionSampleRate: 100,
-  },
-  datadogContext: {
-    app: 'pm-empower',
-    host: window.location.hostname,
-  },
+  datadog_env: 'production',
 };
--- site/src/environments/environment.qa.ts
+++ site/src/environments/environment.qa.ts
@@ -1,42 +1,7 @@
-import { AuthConfig } from '@auth0/auth0-angular';
-
 export const environment = {
   production: true,
   appTitle: 'PM Empower',
   apiPrefix: 'https://api-test.pm-empower.com/api/',
-  appInsightsKey: '44521f2e-90b6-4be0-8659-0c84bfde1907',
-  auth: <AuthConfig>{
-    domain: 'auth.dev.thewbsproject.com',
-    clientId: '1nOKv3soErPVx2nJlfZFZdIqYkjUnMkX',
-    authorizationParams: {
-      audience: 'https://dev-3lcexa83.us.auth0.com/api/v2/',
-      redirect_uri: `${window.location.protocol}//${window.location.host}`,
-    },
-    // The AuthHttpInterceptor configuration
-    httpInterceptor: {
-      allowedList: [
-        {
-          uri: 'https://api-test.pm-empower.com/api/resources/*',
-          allowAnonymous: true,
-        },
-        {
-          uri: 'https://api-test.pm-empower.com/api/lists/*',
-          allowAnonymous: true,
-        },
-        'https://api-test.pm-empower.com/api/*',
-      ],
-    },
-  },
-  datadog: {
-    clientToken: 'pub3cd763417a98f3cb71ee3ab8d19ddecb',
-    site: 'datadoghq.com',
-    service: 'pm-empower-site',
-    env: 'test',
-    forwardErrorsToLogs: true,
-    sessionSampleRate: 100,
-  },
-  datadogContext: {
-    app: 'pm-empower',
-    host: window.location.hostname,
-  },
+  auth_clientId: '1nOKv3soErPVx2nJlfZFZdIqYkjUnMkX',
+  datadog_env: 'test',
 };
