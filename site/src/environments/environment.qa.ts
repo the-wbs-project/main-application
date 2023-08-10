59efcacd8edc8d1992868baf17cda9ce2ed498e0
Christopher Walton <chriswalton@cwsoftware.biz>
2023-08-10 14:15:46 +0700

    another wrong config

--- site/src/environments/environment.qa.ts
+++ site/src/environments/environment.qa.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   appTitle: 'PM Empower',
   apiPrefix: 'https://api-test.pm-empower.com/api/',
-  auth_clientId: '1nOKv3soErPVx2nJlfZFZdIqYkjUnMkX',
+  auth_clientId: 'zfvKgtPltxHsUGNPMYvF6JRljlp6xDuC',
   datadog_env: 'test',
 };
