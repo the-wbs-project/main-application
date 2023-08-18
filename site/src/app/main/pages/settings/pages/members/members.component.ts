c1efb693c66ff82424f3644ca21eef128d4b770f
Christopher Walton <chriswalton@cwsoftware.biz>
2023-08-18 10:18:19 +0700

    minor member updates

--- site/src/app/main/pages/settings/pages/members/members.component.ts
+++ site/src/app/main/pages/settings/pages/members/members.component.ts
@@ -6,22 +6,30 @@ import {
   signal,
 } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { Store } from '@ngxs/store';
 import { DropDownButtonModule } from '@progress/kendo-angular-buttons';
+import { DropDownListModule } from '@progress/kendo-angular-dropdowns';
 import { SVGIconModule } from '@progress/kendo-angular-icons';
 import {
   CompositeFilterDescriptor,
   FilterDescriptor,
   State,
 } from '@progress/kendo-data-query';
-import { gearIcon, pencilIcon, xIcon } from '@progress/kendo-svg-icons';
+import {
+  gearIcon,
+  pencilIcon,
+  plusIcon,
+  xIcon,
+} from '@progress/kendo-svg-icons';
 import { Member } from '@wbs/core/models';
 import { RemoveMemberFromOrganization } from '@wbs/main/actions';
 import { RoleListPipe } from '@wbs/main/pipes/role-list.pipe';
 import { DialogService } from '@wbs/main/services';
-import { first, map } from 'rxjs/operators';
+import { MembershipState } from '@wbs/main/states';
+import { first } from 'rxjs/operators';
 import { ChangeBreadcrumbs } from '../../actions';
 import { UserActionsComponent } from '../../components/user-actions/user-actions.component';
 import { Breadcrumb } from '../../models';
@@ -30,12 +38,11 @@ import { SortArrowComponent } from './components/sort-arrow.component';
 import { TableHelper } from './table-helper.service';
 import { TableProcessPipe } from './table-process.pipe';
 import { SortableDirective } from './table-sorter.directive';
-import { MembershipState } from '@wbs/main/states';
 
 const ROLES = [
   {
     name: 'all',
-    text: 'General.All',
+    text: 'OrgSettings.AllRoles',
   },
   {
     name: 'pm',
@@ -74,7 +81,9 @@ const actionMenu: any[] = [
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     DropDownButtonModule,
+    DropDownListModule,
     EditMemberComponent,
+    FormsModule,
     NgClass,
     NgIf,
     NgFor,
@@ -89,7 +98,6 @@ const actionMenu: any[] = [
   providers: [TableHelper],
 })
 export class MembersComponent implements OnInit {
-  private textFilter = '';
   private readonly crumbs: Breadcrumb[] = [
     {
       text: 'General.Users',
@@ -101,10 +109,12 @@ export class MembersComponent implements OnInit {
     sort: [{ field: 'name', dir: 'asc' }],
   });
   readonly gearIcon = gearIcon;
+  readonly plusIcon = plusIcon;
   readonly menu = actionMenu;
   readonly roles = ROLES;
 
   role = 'all';
+  textFilter = '';
 
   constructor(
     private readonly dialogService: DialogService,
@@ -114,16 +124,6 @@ export class MembersComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.dispatch(new ChangeBreadcrumbs(this.crumbs));
-  }
-
-  setTextFilter(filter: string): void {
-    this.textFilter = filter;
-    this.updateState();
-  }
-
-  setRole(role: string): void {
-    this.role = role;
-    this.updateState();
   }
 
   userActionClicked(member: Member, action: string): void {
@@ -150,8 +150,10 @@ export class MembersComponent implements OnInit {
     }
   }
 
-  private updateState(): void {
-    const state = <State>{};
+  updateState(): void {
+    const state = <State>{
+      sort: this.state().sort,
+    };
     const filters: (CompositeFilterDescriptor | FilterDescriptor)[] = [];
 
     if (this.textFilter) {
