8c60950b979a39fcb14191a0e489231c376c0f0c
Christopher Walton <chriswalton@cwsoftware.biz>
2023-08-20 15:02:47 +0700

    adding my potential API shell

---
+++ api/models/server.ts
@@ -0,0 +1,43 @@
+import express, { Application } from "express";
+import config from "../config";
+import departmentsRoutes from "../routes/departments";
+import cors from "cors";
+import db from "../db/connection";
+
+class Server {
+  private app: Application;
+
+  constructor() {
+    this.app = express();
+    this.dbConnection();
+    this.middlewares();
+    this.routes();
+  }
+
+  async dbConnection() {
+    try {
+      await db.authenticate();
+      console.log("Database connected");
+    } catch (error: any) {
+      console.error(error, "Error connecting to DB");
+    }
+  }
+
+  middlewares() {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.use(config.apiPaths.departments, departmentsRoutes);
+  }
+
+  listen() {
+    this.app.listen(config.port, () => {
+      console.log(`Server up and running at port: ${config.port}`);
+    });
+  }
+}
+
+export default Server;
