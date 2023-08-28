4f02d501e0c7f126d45013716435a04d0b742df9
Christopher Walton <chriswalton@cwsoftware.biz>
2023-08-28 09:29:29 +0700

    fixing config

--- auth-api/Configurations/Auth0Config.cs
+++ auth-api/Configurations/Auth0Config.cs
@@ -9,9 +9,9 @@ public class Auth0Config
 
     public Auth0Config(IConfiguration config)
     {
-        Domain = config["Auth0:Domain"];
-        Audience = config["Auth0:Audience"];
-        ClientId = config["Auth0:ClientId"];
-        ClientSecret = config["Auth0:ClientSecret"];
+        Domain = config["Domain"];
+        Audience = config["Audience"];
+        ClientId = config["ClientId"];
+        ClientSecret = config["ClientSecret"];
     }
 }
