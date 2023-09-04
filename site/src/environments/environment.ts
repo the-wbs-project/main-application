a0f878169b66f54d8d82648173b29f491cb32386
Christopher Walton <chriswalton@cwsoftware.biz>
2023-09-04 09:39:44 +0700

    using new dev auth0 app

--- site/src/environments/environment.ts
+++ site/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   appTitle: 'PM Empower',
   apiPrefix: 'http://localhost:88/api/',
-  auth_clientId: 'zfvKgtPltxHsUGNPMYvF6JRljlp6xDuC',
+  auth_clientId: '44pcQ5Xo33vimvBb4G8eZfjFaGIsyWfg',
   datadog_env: 'development',
 };
