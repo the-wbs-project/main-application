abccf7cb1b5c2f30cfecebd109865a1834ded40e
Christopher Walton <chriswalton@cwsoftware.biz>
2023-09-26 08:43:35 +0700

    fixed site to test the domain change

--- site/src/environments/environment.qa.ts
+++ site/src/environments/environment.qa.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   appTitle: 'PM Empower',
-  apiPrefix: 'https://test-api.pm-empower.com/api/',
+  apiPrefix: 'https://test.pm-empower.com/api/',
   auth_clientId: 'zfvKgtPltxHsUGNPMYvF6JRljlp6xDuC',
   datadog_env: 'test',
 };
