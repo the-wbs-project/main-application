f778fef028153f0e5b020e515d9b9215298806a5
Christopher Walton <chriswalton@cwsoftware.biz>
2024-03-20 10:47:46 +0700

    cleaning up discipline legend

--- site/src/app/main/components/tree-discipline-legend/tree-discipline-legend.component.ts
+++ site/src/app/main/components/tree-discipline-legend/tree-discipline-legend.component.ts
@@ -1,9 +1,17 @@
 import { NgClass } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostListener,
+  input,
+  signal,
+  viewChild,
+} from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCircleQuestion } from '@fortawesome/pro-duotone-svg-icons';
-import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
+import { PopupModule } from '@progress/kendo-angular-popup';
 import { ProjectCategory } from '@wbs/core/models';
 import { DisciplineIconPipe } from '@wbs/main/pipes/discipline-icon.pipe';
 import { DisciplineLabelPipe } from '@wbs/main/pipes/discipline-label.pipe';
@@ -12,19 +20,45 @@ import { DisciplineLabelPipe } from '@wbs/main/pipes/discipline-label.pipe';
   standalone: true,
   selector: 'wbs-tree-discipline-legend',
   templateUrl: './tree-discipline-legend.component.html',
-  styleUrls: ['./tree-discipline-legend.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     DisciplineIconPipe,
     DisciplineLabelPipe,
     FontAwesomeModule,
-    NgbPopoverModule,
     NgClass,
+    PopupModule,
     TranslateModule,
   ],
 })
 export class TreeDisciplineLegendComponent {
-  readonly idsOrCats = input<ProjectCategory[] | undefined>();
-
   readonly faCircleQuestion = faCircleQuestion;
+  readonly show = signal(false);
+  readonly idsOrCats = input<ProjectCategory[] | undefined>();
+  readonly anchor = viewChild<ElementRef>('anchor');
+  readonly popup = viewChild<ElementRef>('popup');
+
+  toggle(): void {
+    this.show.update((show) => !show);
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  public keydown(event: KeyboardEvent): void {
+    if (event.code === 'Escape') {
+      this.toggle();
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  public documentClick(event: KeyboardEvent): void {
+    if (!this.contains(event.target)) {
+      this.toggle();
+    }
+  }
+
+  private contains(target: EventTarget | null): boolean {
+    return (
+      this.anchor()?.nativeElement.contains(target) ||
+      (this.popup ? this.popup()?.nativeElement.contains(target) : false)
+    );
+  }
 }
