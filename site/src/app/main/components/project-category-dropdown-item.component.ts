ac28802063cfdf1df96b95efaf236cbdbfbb6ab2
Christopher Walton <chriswalton@cwsoftware.biz>
2024-04-02 12:27:00 +0700

    cleaned up project categories

--- site/src/app/main/components/project-category-dropdown-item.component.ts
+++ site/src/app/main/components/project-category-dropdown-item.component.ts
@@ -8,19 +8,19 @@ import { ListItem } from '@wbs/core/models';
   selector: 'wbs-project-category-dropdown-item',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `@if (item(); as dataItem) {
-    <div class="d-flex flex-flow-row w-100">
-      <div class="pd-t-15 mg-r-10">
+    <div class="d-flex flex-flow-row w-100 lh-3">
+      <div class="pd-t-10 pd-r-15 mn-ht-50">
         <img
-          class="wd-40 hd-40"
+          class="wd-30 hd-30"
           [src]="dataItem.icon"
           [alt]="dataItem.label | translate"
         />
       </div>
-      <div class="flex-grow-1 pd-10">
-        <span class="d-block tx-semibold">
+      <div class="flex-grow-1 pd-y-5">
+        <span class="d-block tx-semibold pd-0">
           {{ dataItem.label | translate }}
         </span>
-        <span class="tx-10">
+        <span class="tx-10 ">
           {{ dataItem.description! | translate }}
         </span>
       </div>
