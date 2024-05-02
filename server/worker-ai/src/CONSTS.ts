09cd778bf84ccdc20b296fbb652ccd241cbad074
Christopher Walton <chriswalton@cwsoftware.biz>
2024-05-02 08:01:34 +0700

    added ai models

--- server/worker-ai/src/CONSTS.ts
+++ server/worker-ai/src/CONSTS.ts
@@ -1,8 +1,8 @@
 export const TEXT_MODELS = [
 	{
-		model: 'gpt-3.5-turbo',
+		model: 'gpt-4-turbo',
 		type: 'open-ai',
-		title: 'OpenAI ChatGPT 3.5 Turbo',
+		title: 'OpenAI ChatGPT 4 Turbo',
 	},
 	{
 		model: 'gpt-4',
@@ -10,26 +10,23 @@ export const TEXT_MODELS = [
 		title: 'OpenAI ChatGPT 4',
 	},
 	{
-		model: 'gpt-4-turbo-preview',
+		model: 'gpt-3.5-turbo',
 		type: 'open-ai',
-		title: 'OpenAI ChatGPT 4 Turbo Preview',
+		title: 'OpenAI ChatGPT 3.5 Turbo',
 	},
 	{
-		model: '@cf/meta/llama-2-7b-chat-int8',
+		model: '@cf/meta/llama-2-7b-chat-fp16',
 		type: 'worker-ai',
 		title: 'Meta Llama 2.7b',
 	},
+	{
+		model: '@cf/meta/llama-3-8b-instruct',
+		type: 'worker-ai',
+		title: 'Meta Llama 3 (Beta)',
+	},
+	{
+		model: '@cf/mistral/mistral-7b-instruct-v0.1',
+		type: 'worker-ai',
+		title: 'Mistral 7b',
+	},
 ];
-
-export type OPEN_AI_MODELS =
-	| 'gpt-4'
-	| 'gpt-4-0314'
-	| 'gpt-4-0613'
-	| 'gpt-4-32k'
-	| 'gpt-4-32k-0314'
-	| 'gpt-4-32k-0613'
-	| 'gpt-3.5-turbo'
-	| 'gpt-3.5-turbo-16k'
-	| 'gpt-3.5-turbo-0301'
-	| 'gpt-3.5-turbo-0613'
-	| 'gpt-3.5-turbo-16k-0613';
