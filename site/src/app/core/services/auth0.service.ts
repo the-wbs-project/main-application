2f0629dbf67d265993613e6bd733add4b85e35f1
Christopher Walton <chriswalton@cwsoftware.biz>
2024-06-22 12:04:49 -0400

    cleaning upgetting user and org names

--- site/src/app/core/services/auth0.service.ts
+++ site/src/app/core/services/auth0.service.ts
@@ -3,7 +3,12 @@ import { AuthService } from '@auth0/auth0-angular';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { DataServiceFactory } from '@wbs/core/data-services';
 import { User } from '@wbs/core/models';
-import { Logger, Messages } from '@wbs/core/services';
+import {
+  Logger,
+  Messages,
+  OrganizationService,
+  UserService,
+} from '@wbs/core/services';
 import { AiStore, MembershipStore, UserStore } from '@wbs/core/store';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { switchMap, tap } from 'rxjs/operators';
@@ -18,6 +23,8 @@ export class Auth0Service {
   private readonly membership = inject(MembershipStore);
   private readonly messages = inject(Messages);
   private readonly userStore = inject(UserStore);
+  private readonly userService = inject(UserService);
+  private readonly organizationService = inject(OrganizationService);
 
   private readonly _isInitiated = new BehaviorSubject<boolean>(false);
 
@@ -43,6 +50,10 @@ export class Auth0Service {
 
       this.aiStore.setUserInfo(profile);
       this.userStore.set(profile);
+      this.userService.addUsers([profile]);
+      this.organizationService.addOrganizations(
+        user[ns + '/organizations'] ?? []
+      );
       this.logger.setGlobalContext({
         'usr.id': profile.id,
         'usr.name': profile.name,
