d1922dce404e9ae1f36c805bc9d5c117a632fb55
Christopher Walton <chriswalton@cwsoftware.biz>
2024-06-25 08:28:43 -0400

    improving draft indicating

--- site/src/app/pipes/library-status.pipe.ts
+++ site/src/app/pipes/library-status.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'libraryStatus', standalone: true })
 export class LibraryStatusPipe implements PipeTransform {
   transform(status: string | undefined): string {
-    if (status === 'draft') return 'General.Draft';
+    if (status === 'draft') return 'General.UnderConstruction';
     if (status === 'published') return 'General.Published';
     if (status === 'retired') return 'General.Retired';
     return '';
