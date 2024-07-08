1e8c3daa3803de065a5d4e465a3ad0648867683c
Christopher Walton <chriswalton@cwsoftware.biz>
2024-07-08 06:28:59 -0500

    finished initial UI

--- site/src/app/pages/library/pages/entry-view/components/visibility-icon.component.ts
+++ site/src/app/pages/library/pages/entry-view/components/visibility-icon.component.ts
@@ -1,23 +1,31 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faLock } from '@fortawesome/pro-solid-svg-icons';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   standalone: true,
   selector: 'wbs-visibility-icon',
-  template: `@if (visibility() === 'private') {
-    <i class="fas fa-lock" [title]="'Wbs.PrivateTooltip' | translate"></i> }
-    @else if (visibility() === 'impliedPrivate') {
-    <i
-      class="fas fa-lock tx-gray-400"
-      [title]="'Wbs.ImpliedPrivateTooltip' | translate"
-    ></i>
-    }`,
+  template: `@if (iconClass(); as iconClass) {
+    <i [class]="iconClass" [title]="tooltip() | translate"></i> }`,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [FontAwesomeModule, TranslateModule],
+  imports: [TranslateModule],
 })
 export class VisibilityIconComponent {
-  readonly lockIcon = faLock;
   readonly visibility = input.required<string | undefined>();
+  readonly iconClass = computed(() =>
+    this.visibility() === 'private'
+      ? 'fas fa-lock'
+      : this.visibility() === 'impliedPrivate'
+      ? 'fas fa-lock tx-gray-400'
+      : undefined
+  );
+  readonly tooltip = computed(() =>
+    this.visibility() === 'private'
+      ? 'Wbs.PrivateTooltip'
+      : 'Wbs.ImpliedPrivateTooltip'
+  );
 }
