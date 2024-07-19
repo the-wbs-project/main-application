5b23607bb41eb00395b76575bad6922b36009420
Christopher Walton <chriswalton@cwsoftware.biz>
2024-07-19 12:05:41 +0700

    working on build

--- server/worker/src/services/http-services/organization.http-service.ts
+++ server/worker/src/services/http-services/organization.http-service.ts
@@ -4,8 +4,11 @@ export class OrganizationHttpService {
   static async getByNameAsync(ctx: Context): Promise<Response> {
     try {
       const { organization } = ctx.req.param();
+      const org = await ctx.get('data').organizations.getAsync(organization);
 
-      return ctx.json(await ctx.get('data').organizations.getNameAsync(organization));
+      if (!org) return ctx.text('Not Found', 404);
+
+      return ctx.json(org?.displayName);
     } catch (e) {
       ctx.get('logger').trackException('An error occured trying to get an organization.', <Error>e);
 
