158d729666d4bcfc9525c2568fcacfff4d9c3289
Christopher Walton <chriswalton@cwsoftware.biz>
2024-08-09 07:28:15 +0700

    updated avatar in header

--- site/src/app/layout/components/header-profile-picture.component.ts
+++ site/src/app/layout/components/header-profile-picture.component.ts
@@ -1,35 +1,20 @@
 import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faUserCircle } from '@fortawesome/pro-solid-svg-icons';
+import { AvatarModule } from '@progress/kendo-angular-layout';
+import { userIcon } from '@progress/kendo-svg-icons';
 
 @Component({
   standalone: true,
   selector: 'wbs-header-profile-picture',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [FontAwesomeModule, NgClass],
-  template: `@if (picture()) {
-    <img [src]="picture()" class="profile-img" />
+  imports: [AvatarModule, NgClass],
+  template: `@if (picture(); as pic) {
+    <kendo-avatar [imageSrc]="pic" [classList]="['profile-img']" />
     } @else {
-    <fa-icon
-      [icon]="faUserCircle"
-      style="font-size: 1.2rem"
-      size="2x"
-      [ngClass]="'profile-img'"
-    />
+    <kendo-avatar [svgIcon]="userIcon" [classList]="['profile-img']" />
     }`,
-  styles: [
-    `
-      .profile-img {
-        width: 37px;
-        height: 37px;
-        border-radius: 50%;
-        box-shadow: 0px 4px 4px 0px #dbe4f9;
-      }
-    `,
-  ],
 })
 export class HeaderProfilePictureComponent {
   readonly picture = input.required<string | undefined>();
-  readonly faUserCircle = faUserCircle;
+  readonly userIcon = userIcon;
 }
